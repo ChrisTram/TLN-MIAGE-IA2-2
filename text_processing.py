c9211b1b7b4dcbb6be9c314d44b0326cc828d05f
Mael Giese <maelgiese06@gmail.com>
2020-11-26 12:28:08 +0100

    Text processing file Named entity spacy model

---
+++ text_processing.py
@@ -0,0 +1,20 @@
+from spacy_model.en_core_web_sm import en_core_web_sm
+
+
+def get_named_entity(text):
+    nlp = en_core_web_sm.load()
+    doc = nlp(text)
+
+    named_entity = []
+
+    for ent in doc.ents:
+        # print(ent.text, ent.label_)
+        ne = ent.text
+        named_entity.append(ne)
+
+    return named_entity
+
+
+text = 'The United States'
+
+print(get_named_entity(text))

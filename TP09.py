41ccf5e33cee8b23991ccc12008ce8468cac8092
Mael Giese <maelgiese06@gmail.com>
2020-12-03 12:59:52 +0100

    Classification model

--- TP09.py
+++ TP09.py
@@ -14,7 +14,7 @@ import pandas as pd
 def get_datas(xmlPath):
     sentences = []
     terms = []
-    polarities = []
+    polarity = []
     sents_scores = []
     fromList = []
     toList = []
@@ -28,22 +28,20 @@ def get_datas(xmlPath):
         # We only keep datas with terms
         if len(aspect_terms) > 0:
             for term in aspect_terms:
-                # We dont keep the term with 'conflict' label
-                if term.getAttribute("polarity") != 'conflict':
-                    # Add all features to the lists
-                    # For each term we had the full sentence as a features
-                    sentences.append(sentence.firstChild.data)
-                    terms.append(term.getAttribute("term"))
-                    polarities.append(term.getAttribute("polarity"))
-                    sents_scores.append(get_sentiment_score(term.getAttribute("term")))
-                    fromList.append(term.getAttribute("from"))
-                    toList.append(term.getAttribute("to"))
+                # Add all features to the lists
+                # For each term we had the full sentence as a feature
+                sentences.append(sentence.firstChild.data)
+                terms.append(term.getAttribute("term"))
+                polarity.append(term.getAttribute("polarity"))
+                sents_scores.append(get_sentiment_score(term.getAttribute("term")))
+                fromList.append(term.getAttribute("from"))
+                toList.append(term.getAttribute("to"))
 
             # Create a Dataframe from all lists
             df = pd.DataFrame(sentences, columns=['Sentences'])
             df['Terms'] = terms
             df['Sentiments_Scores'] = sents_scores
-            df['Polarities'] = polarities
+            df['polarity'] = polarity
             df['From'] = fromList
             df['To'] = toList
 
@@ -51,16 +49,23 @@ def get_datas(xmlPath):
 
 
 def data_pre_treatment(df):
-    # We remove
-    label = df['Polarities']
+    # We remove the term with 'conflict' label
+    df = df[df['polarity'] != 'conflict']
 
-    return label
+    # convert polarity to numeric
+    label = df['polarity'].factorize()
+
+    # We dont need the label in the dataframe anymore
+    df.drop(['polarity'], axis=1)
+
+    return df, label
 
 
 if __name__ == "__main__":
+    # df_rest = get_datas('.\Dataset\Restaurants_Test_Gold.xml')
     df_rest = get_datas('.\Dataset\Restaurants_Train.xml')
     df_lap = get_datas('.\Dataset\Laptop_Train.xml')
 
+    df_rest, label = data_pre_treatment(df_rest)
     print(df_rest)
-
-    #print(data_pre_treatment(df_rest))
+    print(label)
--- model.py
+++ model.py
@@ -1,21 +1,39 @@
 import tensorflow as tf
 import TP09
-
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 if __name__ == "__main__":
     df = TP09.get_datas('.\Dataset\Restaurants_Train.xml')
+    df, label = TP09.data_pre_treatment(df)
 
-    VOCAB_SIZE=1000
-    encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(
-        max_tokens=VOCAB_SIZE)
-    encoder.adapt(train_dataset.map(lambda text, label: text))
+    text = df['Sentences'].values
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(text)
+    vocab_size = len(tokenizer.word_index) + 1
+    encoded_docs = tokenizer.texts_to_sequences(text)
+    padded_sequence = pad_sequences(encoded_docs, maxlen=200)
+
+    print(tokenizer.word_index)
+
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import LSTM, Dense, Dropout
+    from tensorflow.keras.layers import SpatialDropout1D
+    from tensorflow.keras.layers import Embedding
+
+    embedding_vector_length = 64
 
     model = tf.keras.Sequential([
-        encoder,
-        tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
-        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+        tf.keras.layers.Embedding(vocab_size, embedding_vector_length, mask_zero=True),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(1)
     ])
+
+    model.compile(loss='binary_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+
+    history = model.fit(padded_sequence, label[0],
+                        validation_split=0.2, epochs=5, batch_size=32)

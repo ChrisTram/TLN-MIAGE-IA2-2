77fe2d58ec21e65ca3f0cf6bfc2eef285e8a8db2
Chris <chris.tramier97@gmail.com>
2020-12-17 10:38:04 +0100

    minor update

--- TP09.py
+++ TP09.py
@@ -62,6 +62,46 @@ def data_pre_treatment(df):
 
     return df, label
 
+def make_prediction_noLabel(path):
+    df = get_datas('.\Dataset\\' + path + '_Test_NoLabels.xml')
+
+    # Load the model
+    saved_model = tf.keras.models.load_model(path + '_model.h5')
+
+    df, label = data_pre_treatment(df)
+
+    maxlen = 100
+
+    # Sentences to bag of words
+    text = df['Sentences'].values
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(text)
+    vocab_size = len(tokenizer.word_index) + 1
+    encoded_docs = tokenizer.texts_to_sequences(text)
+    text_padded_sequence = pad_sequences(encoded_docs, maxlen=maxlen)
+
+    # Terms to bag of words, we use the same tokenizer we got from the sentences
+    terms = df['Terms'].values
+    encoded_docs = tokenizer.texts_to_sequences(terms)
+    terms_padded_sequence = pad_sequences(encoded_docs, maxlen=10)
+
+    # Append the different features to the bag of words
+    features = model.append_feature_to_vector(text_padded_sequence, terms_padded_sequence)
+    features = model.append_feature_to_vector(features, df['Sentiments_Scores'].to_numpy())
+
+    features = np.array(features)
+
+    # Predict the entire dataset
+    predictions = saved_model.predict(features)
+    print("""
+    //////////////////////////////////////////////////
+    //////////////////// PREDICTION //////////////////
+    //////////////////////////////////////////////////
+    """)
+    print(path)
+    # Evaluate the model
+    print(saved_model.evaluate(predictions, label[0]))
+
 
 def make_prediction(path):
     df = get_datas('.\Dataset\\' + path + '_Test_Gold.xml')
@@ -106,9 +146,10 @@ def make_prediction(path):
 
 if __name__ == "__main__":
 
-    ##### Change the path to load the rigth model
     path_r = 'Restaurants'
     path_l = 'Laptop'
 
     make_prediction(path_r)
+    
     make_prediction(path_l)
+    make_prediction_noLabel(path_l)

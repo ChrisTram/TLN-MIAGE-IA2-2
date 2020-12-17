38763906f8ac525057ddf90986f5983dc4753f19
MaelGiese <55782726+MaelGiese@users.noreply.github.com>
2020-12-17 17:47:24 +0100

    Update TP09.py

--- TP09.py
+++ TP09.py
@@ -48,8 +48,6 @@ def get_datas(xmlPath):
     return df
 
 
-
-    
 def data_pre_treatment(df):
     # We remove the term with 'conflict' label
     df = df[df['polarity'] != 'conflict']
@@ -62,19 +60,8 @@ def data_pre_treatment(df):
 
     return df, label
 
-def make_prediction_noLabel(path):
-    df = get_datas('.\Dataset\\' + path + '_Test_NoLabels.xml')
 
-    # Load the model
-    saved_model = tf.keras.models.load_model(path + '_model.h5')
-
-    df, label = data_pre_treatment(df)
-
-    # Load data with label
-    dfLabel = get_datas('.\Dataset\\' + path + '_Test_Gold.xml')
-
-    dfLabel, label_train = data_pre_treatment(df)
-
+def make_prediction_noLabel(df, saved_model):
     maxlen = 100
 
     # Sentences to bag of words
@@ -98,63 +85,50 @@ def make_prediction_noLabel(path):
 
     # Predict the entire dataset
     predictions = saved_model.predict(features)
-    print("""
-    //////////////////////////////////////////////////
-    //////////////////// PREDICTION //////////////////
-    //////////////////////////////////////////////////
-    """)
-    print(path)
-    # Evaluate the model
-    print(saved_model.evaluate(predictions, label_train[0]))
 
-
-def make_prediction(path):
-    df = get_datas('.\Dataset\\' + path + '_Test_Gold.xml')
-
-    # Load the model
-    saved_model = tf.keras.models.load_model(path + '_model.h5')
-
-    df, label = data_pre_treatment(df)
-
-    maxlen = 100
-
-    # Sentences to bag of words
-    text = df['Sentences'].values
-    tokenizer = Tokenizer(num_words=5000)
-    tokenizer.fit_on_texts(text)
-    vocab_size = len(tokenizer.word_index) + 1
-    encoded_docs = tokenizer.texts_to_sequences(text)
-    text_padded_sequence = pad_sequences(encoded_docs, maxlen=maxlen)
-
-    # Terms to bag of words, we use the same tokenizer we got from the sentences
-    terms = df['Terms'].values
-    encoded_docs = tokenizer.texts_to_sequences(terms)
-    terms_padded_sequence = pad_sequences(encoded_docs, maxlen=10)
-
-    # Append the different features to the bag of words
-    features = model.append_feature_to_vector(text_padded_sequence, terms_padded_sequence)
-    features = model.append_feature_to_vector(features, df['Sentiments_Scores'].to_numpy())
-
-    features = np.array(features)
-
-    # Predict the entire dataset
-    predictions = saved_model.predict(features)
-    print("""
-    //////////////////////////////////////////////////
-    //////////////////// PREDICTION //////////////////
-    //////////////////////////////////////////////////
-    """)
-    print(path)
-    # Evaluate the model
-    print(saved_model.evaluate(predictions, label[0]))
+    return predictions
 
 
 if __name__ == "__main__":
-
+    ######## Evaluation des fichier Restaurants
     path_r = 'Restaurants'
+
+    # Load the dataframe without the label
+    df_no_label = get_datas('.\Dataset\\' + path_r + '_Test_NoLabels.xml')
+    df_no_label, _ = data_pre_treatment(df_no_label)
+
+    # Load data with label
+    df_Label = get_datas('.\Dataset\\' + path_r + '_Test_Gold.xml')
+    _, label_gold = data_pre_treatment(df_Label)
+
+    # Load the model
+    saved_model = tf.keras.models.load_model(path_r + '_model.h5')
+
+    # On fait la prediction sur les données du fichier Test_NoLabels
+    r_predictions = make_prediction_noLabel(df_no_label, saved_model)
+
+    # Evaluate the model
+    r_metrics = saved_model.evaluate(r_predictions, label_gold[0])
+    print(str(saved_model.metrics_names[1]) + ': ' + str(r_metrics[1]))
+
+
+    ######## Evaluation des fichier Laptop
     path_l = 'Laptop'
 
-    make_prediction(path_r)
-    
-    make_prediction(path_l)
-    make_prediction_noLabel(path_l)
+    # Load the dataframe without the label
+    df_no_label = get_datas('.\Dataset\\' + path_l + '_Test_NoLabels.xml')
+    df_no_label, _ = data_pre_treatment(df_no_label)
+
+    # Load data with label
+    df_Label = get_datas('.\Dataset\\' + path_l + '_Test_Gold.xml')
+    _, label_gold = data_pre_treatment(df_Label)
+
+    # Load the model
+    saved_model = tf.keras.models.load_model(path_l + '_model.h5')
+
+    # On fait la prediction sur les données du fichier Test_NoLabels
+    l_predictions = make_prediction_noLabel(df_no_label, saved_model)
+
+    # Evaluate the model
+    l_metrics = saved_model.evaluate(l_predictions, label_gold[0])
+    print(str(saved_model.metrics_names[1]) + ': ' + str(l_metrics[1]))
